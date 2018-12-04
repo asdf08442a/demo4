2666d178dbcf7967b6ee134b0815ee4f058875e8
jinzhengang <jinzhengang@didichuxing.com>
2018-12-04 10:33:44 +0800

    init

--- portal/src/main/java/com/enterprise/demo/portal/controller/TestController.java
+++ portal/src/main/java/com/enterprise/demo/portal/controller/TestController.java
@@ -2,12 +2,10 @@ package com.enterprise.demo.portal.controller;
 
 import com.enterprise.demo.permission.dao.PermissionEntity;
 import com.enterprise.demo.permission.service.PermissionService;
-import com.enterprise.demo.portal.dto.RequestDTO;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +22,7 @@ public class TestController {
   private PermissionService permissionService;
 
   @PostMapping("/permissions")
-  public List<PermissionEntity> sayHello(@RequestBody RequestDTO requestDTO) {
-    log.info("request:{}", requestDTO);
+  public List<PermissionEntity> sayHello() {
     return permissionService.selectAll();
   }
 
